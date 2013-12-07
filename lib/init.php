dcd85d1dee3a357ed747289b55bfd71faa9dfa39
Ahmed Ali <ahmed@enfusionweb.com>
2013-12-07 14:46:12 +0500

    File for Initializing the Framework files

--- lib/init.php
+++ lib/init.php
@@ -4,3 +4,9 @@
 * this file under any circumstances. Please do all modifications
 * in the form of a child theme.
 **/
+
+require_once( dirname( __FILE__ ) . '/framework.php' );
+require_once( dirname( __FILE__ ) . '/widgets/widgets.php');
+require_once( dirname( __FILE__ ) . '/menus/menus.php');
+require_once( dirname( __FILE__ ) . '/functions/main.php' );
+require_once( dirname( __FILE__ ) . '/functions/header.php' );
