9e5575d523e91242a3b579e81a5a53656c7c7b4f
Ahmed Ali <ahmed@enfusionweb.com>
2013-12-07 14:47:00 +0500

    Registering Menus

---
+++ lib/menus/menus.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* WARNING: This file is part of the core Adam Framework. DO NOT edit
+* this file under any circumstances. Please do all modifications
+* in the form of a child theme.
+**/
+
+
+register_nav_menu( 'primary', __( 'Primary Menu', 'twentyeleven' ) );
