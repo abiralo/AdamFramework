8d4afd5628f650269e0d1cbe992fe2542c4ab436
Ahmed Ali <ahmed@enfusionweb.com>
2013-12-07 14:47:13 +0500

    Shortcodes files for all shortcodes

---
+++ lib/shortcodes/shortcodes.php
@@ -0,0 +1,6 @@
+<?php
+/**
+* WARNING: This file is part of the core Adam Framework. DO NOT edit
+* this file under any circumstances. Please do all modifications
+* in the form of a child theme.
+**/
